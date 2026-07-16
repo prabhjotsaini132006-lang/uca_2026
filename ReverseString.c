f3de8ac61a57a0742cb11149639474c70c6e40dc
prabhjotsaini132006-lang <prabhjotsaini132006@gmail.com>
2026-07-16 08:51:42 +0000

    Add in-place string reversal implementation

---
+++ ReverseString.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void reverse_string(char *str) {
+    int start = 0;
+    int end = 0;
+
+    while (str[end] != '\0') {
+        end++;
+    }
+    end--;
+
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    char str[] = "Data Structures";
+
+    reverse_string(str);
+
+    printf("%s\n", str);
+
+    return 0;
+}
